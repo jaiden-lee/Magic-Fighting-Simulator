90301b7936107c0108bd043a8a2d7ebd6b67b5ec
jaiden-lee <96359051+jaiden-lee@users.noreply.github.com>
2022-02-02 22:27:07 -0500

    Create Constants.lua
    This file is where most (if not all) the constants will go - we will organize into 1 module script, so we don't have to dig around in every script.

---
+++ Constants.lua
@@ -0,0 +1,22 @@
+-- this folder will go into replicated storage for both client and server access
+local constants = {}
+
+-- these are constant/final values - make call caps
+constants.SPELL_BASE_DAMAGES = {
+	["SPELL_BOOK_1"] = 100,
+	["SPELL_BOOK_2"] = 100,
+	["SPELL_BOOK_3"] = 100
+}
+
+constants.SPELL_COOLDOWNS = {
+	["SPELL_BOOK_1"] = 3,
+	["SPELL_BOOK_2"] = 3,
+	["SPELL_BOOK_3"] = 3,
+}
+
+-- roblox index starts at 1 - so rank 1 corresponds with 1st element
+constants.LEVEL_DAMAGE_MULTIPLIERS = {1, 1.5, 2, 2.5, 3}
+
+
+
+return constants
