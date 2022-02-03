e94dc876d4b05fb0aaececed9c550bf60e6f9f82
jaiden-lee <96359051+jaiden-lee@users.noreply.github.com>
2022-02-02 22:09:17 -0500

    Create SpellBookClass.lua
    Currently just has a constructor w/ attributes of Name and Level

---
+++ SpellBookClass.lua
@@ -0,0 +1,19 @@
+-- this is a module script - put into serverscriptservice
+local SpellBook = {}
+SpellBook.__index = SpellBook
+SpellBook.__mode = "kv"
+
+
+-- constructor - we are really only using a class for data storage reasons
+function SpellBook.new (spellName)
+	local newBook = {
+		Name = spellName,
+		Level = 1
+	}
+	
+	setmetatable(newBook, SpellBook)
+	
+	return newBook
+end
+
+-- functions: none for now
