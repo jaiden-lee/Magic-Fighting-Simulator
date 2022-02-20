ddb0afccac12982add9cfb55335d110747dc99f1
jaiden-lee <96359051+jaiden-lee@users.noreply.github.com>
2022-02-20 18:35:04 -0500

    Update Constants.lua

--- Constants.lua
+++ Constants.lua
@@ -3,21 +3,29 @@ local constants = {}
 
 -- these are constant/final values - make call caps
 constants.SPELL_BASE_DAMAGES = {
-	["SPELL_BOOK_1"] = 100,
-	["SPELL_BOOK_2"] = 100,
-	["SPELL_BOOK_3"] = 100
+	-- Area 1
+	["Fireball"] = 100,
+	["Water Blast"] = 100,
+	["Rock Smash"] = 100,
+	["Gale Blast"] = 200,
+	["Wood Spikes"] = 200,
+	["Ice Rain"] = 500
 }
 
 constants.SPELL_COOLDOWNS = {
-	["SPELL_BOOK_1"] = 3,
-	["SPELL_BOOK_2"] = 3,
-	["SPELL_BOOK_3"] = 3,
+	-- Area 1
+	["Fireball"] = 3,
+	["Water Blast"] = 3,
+	["Rock Smash"] = 3,
+	["Gale Blast"] = 3,
+	["Wood Spikes"] = 3,
+	["Ice Rain"] = 3
 }
 
 -- roblox index starts at 1 - so rank 1 corresponds with 1st element
 constants.LEVEL_DAMAGE_MULTIPLIERS = {1, 1.5, 2, 2.5, 3}
 
+constants.BASE_SPELLS_EQUIPPED = 4
 
 
 return constants
-
